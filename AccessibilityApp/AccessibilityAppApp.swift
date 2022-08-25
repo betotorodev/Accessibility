6bbb02c62cf33eac967b33eedd684eb87064ec89
Beto Toro <beto.toro@zemoga.com>
2022-08-25 15:00:04 -0500

    Initial Commit

---
+++ AccessibilityApp/AccessibilityAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilityAppApp.swift
+//  AccessibilityApp
+//
+//  Created by Beto Toro on 25/08/22.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilityAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
